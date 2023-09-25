70bc2a26ebeedf3d0adeb03f8626aa0789822dfe
jacobpachev <jacobpachev@gmail.com>
2023-09-25 13:26:27 -0600

    Added a clear feature

--- calculator.js
+++ calculator.js
@@ -4,16 +4,44 @@ new Vue({
 	vuetify: new Vuetify(),
 	data: function() {
 		return {
-			data: [],
-			pos: -200
-			
+			res: '',
+			vis_res: '',
+			past_res: '',
+			pos: -200, 
+			pos_ans: -200,
+			submitted: false,
+			res_len: ''
 		}
     },
 	methods: {
 		place_num: function(n) {
-			this.data.push(n);
-			this.pos += 10;
-			console.log(this.pos);
-		}
+			this.submitted = false;
+			this.res = this.res + n;
+			this.vis_res = this.res;
+			this.past_res = this.res;
+			var res_len = this.res.length*12-200;
+			this.pos = this.pos < 0 ? res_len : this.overflow();
+		},
+		overflow() {
+
+		},
+		submit: function() {
+				this.submitted = true;
+				this.res = eval(this.res);
+				
+		},
+		return_ans: function() {
+				res = this.res;
+				console.log(this.past_res.toString().length) ;
+				var digits = Math.log(res) * Math.LOG10E + 1 | 0;
+				this.pos = digits*12-200;
+				this.pos_ans = (this.past_res.toString().length)*9-220;
+				return(res);
+		},
+        clear: function() {
+            this.vis_res = '';
+            this.res = '';
+            this.past_res = '';
+        }
 	}
 })
